0b58e8b24ee9632077e28ffe63a30803cab73ebb
krauser085 <krauser085@gmail.com>
2016-11-09 20:48:51 +0900

    add moving action on the circle

--- sfml_ex1/sfml_test.cpp
+++ sfml_ex1/sfml_test.cpp
@@ -4,26 +4,35 @@ int main() {
 	// set RenderWindow
 	sf::RenderWindow window(sf::VideoMode(640, 480), "sfml test!!!");
 	
+	float horizontal = 320;
+	float vertical = 240;
 	// set circle
 	sf::CircleShape circle;
-	circle.setRadius(40.f);
-	circle.setPosition(100.f, 100.f);
-	circle.setFillColor(sf::Color::Blue);
+	circle.setRadius(70);
+	circle.setFillColor(sf::Color::Red);
 
 	while (window.isOpen()) {
 
+		// set circle position on this loop to make it move
+		circle.setPosition(horizontal, vertical);
+		
 		// some sort of Event Handler?
 		sf::Event event;
 
 		while (window.pollEvent(event)) {
+
+			horizontal--;
 			
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 		}
 
+		// clear on every frame
 		window.clear(sf::Color::White);
+		// draw the circle on every frame
 		window.draw(circle);
+		// display what window has done on every frame
 		window.display();
 	}
 }
